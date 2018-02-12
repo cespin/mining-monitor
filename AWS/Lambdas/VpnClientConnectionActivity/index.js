5ecd37917d56dd7b57ed8a87ccd37f32d616efd0
Carlos Espin <cespin@touchtunes.com>
2018-02-12 01:24:23 -0500

    VPN Connection Status Lambda

---
+++ AWS/Lambdas/VpnClientConnectionActivity/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+exports.handler = (event, context, callback) => {
+    const AWS = require('aws-sdk');
+
+    console.info('Mine \'' + event.mine + '\' is ' +
+        (event.updateValue ? 'connected' : 'disconnected'));
+
+    const params = {
+        ExpressionAttributeNames: {
+            '#VN': 'vpn'
+        },
+        ExpressionAttributeValues: {
+            ':vn': {
+                M: {
+                    'connected': {
+                        BOOL: event.updateValue
+                    }
+                }
+            }
+        },
+        TableName: 'everything',
+        UpdateExpression: 'SET #VN = :vn',
+        Key: {
+            'name': {
+                S: event.mine
+            }
+        },
+    };
+
+    // Create the DynamoDB service object
+    const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+    // Call DynamoDB to add the item to the table
+    dynamodb.updateItem(params, function (err, data) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        console.info('Update succesful for ' + event.mine);
+    });
+
+    const sns = new AWS.SNS();
+    const accountId = context.invokedFunctionArn.split(':')[4];
+    const topicArn = 'arn:aws:sns:' + AWS.config.region + ':' + accountId + ':vcc-' + event.mine;
+
+    console.info('The ARN: ' + topicArn);
+
+    sns.publish({
+        Message: JSON.stringify({
+            'default': event.mine,
+            'mine': event.mine,
+            'connected': event.updateValue
+        }),
+        TopicArn: topicArn,
+        MessageStructure: 'json'
+    }, function (err, data) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        console.info('SNS notification sent for ' + event.mine);
+    });
+
+    //callback(null, 'Done');
+};
